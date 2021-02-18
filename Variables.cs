15595a21e2334680b0bd81e9a6757ac821e9479d
priyankaparwani3 <me.priyankaparwani@gmail.com>
2021-02-18 13:04:06 +0530

    Add files via upload

---
+++ Variables.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Variables
+{
+    class Variables
+    {
+        static void Main(string[] args)
+        {
+            var number = 256987423;
+            var count = 10;
+            var price = 20.95f;
+            var character = 'A';
+            var firstName = "Priyanka";
+            var working = false;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(price);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(working);
+            Console.WriteLine("{0} {1}", byte.MinValue , byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+            const float pi = 3.14f;
+        
+        }
+
+    }
+}
